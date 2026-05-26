5bbf1b240fcba495574cf82f75a152884cf1add9
whitesh12 <carmen5835@catholic.ac.kr>
2026-05-26 19:40:57 +0900

    Feat: 프론트 UI 코드

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ExcuseCard from './components/ExcuseCard'
 import ExcuseForm from './components/ExcuseForm'
 import Footer from './components/Footer'
 import Sidebar from './components/Sidebar'
+import api from './apl/axios.js'
 import { useState, useEffect } from 'react'
 
 function App() {
