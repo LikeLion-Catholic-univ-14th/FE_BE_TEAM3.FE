a3f16d0f61a46fd699ab3397c0f7f56349e78a1e
whitesh12 <carmen5835@catholic.ac.kr>
2026-05-26 20:10:09 +0900

    fix: 제발 점

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
  import axios from 'axios';
     
      const instance = axios.create({
-       baseURL: import.meta.env.VITE_API_BASE_URL || 'https://localhost:8080'
+       baseURL: import.meta.env.VITE_API_BASE_URL || 'https://fe-be-team-3-fe.vercel.app'
     });
     
      export default instance;
